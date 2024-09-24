2f5e886d7decdf60bafc7bf9cad4ce675a8b97be
steehhh <stefanypradebon@gmail.com>
2024-09-23 22:18:36 -0300

    Add files via upload

---
+++ operador relacional.py
@@ -0,0 +1,16 @@
+num1 = 40
+
+num2 = 50
+
+resposta = "resposta"
+
+resposta = int(input("escolha um desses numeros: 40 ou 50\n"))
+
+if resposta == num1:
+    print("o numero escolhido é menor que 50.")
+
+elif resposta == num2: 
+    print("o numero escolhido é maior que 40.")
+
+else: 
+    print("operação invalida.")
